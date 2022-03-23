fd0fee5bbfb22e0139b58f63fa281b57f3a2f4ad
Edgar Lourenco <up201604910@fc.up.pt>
2022-03-23 08:46:55 +0000

    IA + DA

---
+++ DA/DA_TP_Classes/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/edgarlourenco/Desktop/Faculdade/2ºSemestre/DA/DA_TP_Classes/lib/googletest-master
+# Build directory: /Users/edgarlourenco/Desktop/Faculdade/2ºSemestre/DA/DA_TP_Classes/cmake-build-debug/lib/googletest-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ DA/DA_TP_Classes/cmake-build-debug/lib/googletest-master/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/edgarlourenco/Desktop/Faculdade/2ºSemestre/DA/DA_TP_Classes/lib/googletest-master/googletest
+# Build directory: /Users/edgarlourenco/Desktop/Faculdade/2ºSemestre/DA/DA_TP_Classes/cmake-build-debug/lib/googletest-master/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
